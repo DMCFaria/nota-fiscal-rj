4270baca5c88e636b68b14e87c3af12d067c8683
Daniel Mello <danielmello0110@gmail.com>
2026-03-04 16:48:13 -0300

    ajustes

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext"
+import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth(); // Adicione o loading aqui
+
+  // Enquanto estiver verificando se o user está logado, não redirecione
+  if (loading) {
+    return <div>Carregando...</div>; // Ou um Spinner/Esqueleto de tela
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
